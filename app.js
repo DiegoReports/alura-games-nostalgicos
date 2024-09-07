6115ff086eb44d8ff7d27e86d31124ee96dfad86
Diego Henrique <diego.reports@gmail.com>
2024-09-07 10:40:37 -0300

    Update: adicionando validações de pesquisa - Aula 5

--- app.js
+++ app.js
@@ -1,43 +1,78 @@
-const games = [
-  {
-    title: 'Super Mario World',
-    description:
-      'Um clássico da Nintendo, explorando o Reino dos Cogumelos com o encanador mais famoso do mundo.',
-    image: './assets/marioworld.webp',
-    genre: 'Aventura',
-    console: 'Super Nintendo'
-  },
-  {
-    title: 'Sonic the Hedgehog 2',
-    description:
-      'Acelere com o ouriço azul mais rápido do mundo em uma aventura épica.',
-    image: './assets/sonic2.webp',
-    genre: 'Aventura',
-    console: 'Mega Drive'
-  },
-  {
-    title: 'Street Fighter II',
-    description: 'Lutas de rua para provar os seus limites.',
-    image: './assets/street-fighter.webp',
-    genre: 'Luta',
-    console: 'Super Nintendo'
+/* function pesquisar() {
+  const gameList = document.querySelector('.resultados-pesquisa ul')
+
+  games.forEach(game => {
+    const listItem = document.createElement('li')
+    listItem.classList.add('item-resultado')
+
+    listItem.innerHTML = `
+      <img src="${game.image}" alt="${game.title}" class="jogo-imagem">
+      <h2>${game.title}</h2>
+      <p>${game.description}</p>
+      <span class="genero">${game.genre}</span>
+      <span class="console">${game.console}</span>
+      <a href=${game.link} id="btnInfo" target="_blank">Saiba mais.</a>
+    `
+
+    gameList.appendChild(listItem)
+  })
+} */
+
+function pesquisar() {
+  const gameList = document.querySelector('.resultados-pesquisa ul')
+  const input = document.querySelector('input') // Seleciona o input de pesquisa
+  const pesquisa = input.value.trim().toLowerCase() // Obtém o valor do input e converte para minúsculas para facilitar a comparação
+
+  // Verifica se a pesquisa tem algum valor válido
+  if (pesquisa === '') {
+    // Exibe uma mensagem de erro ou realiza outra ação desejada
+    alert('Por favor, digite um termo de pesquisa válido.')
+    return // Interrompe a função
   }
-]
 
-const gameList = document.querySelector('.resultados-pesquisa ul')
+  // Limpa a lista de resultados antes de cada pesquisa
+  gameList.innerHTML = ''
 
-games.forEach(game => {
-  const listItem = document.createElement('li')
-  listItem.classList.add('item-resultado')
+  // Cria um array para armazenar os resultados da pesquisa
+  const resultados = []
 
-  listItem.innerHTML = `
-    <img src="${game.image}" alt="${game.title}" class="jogo-imagem">
-    <h2>${game.title}</h2>
-    <p>${game.description}</p>
-    <span class="genero">${game.genre}</span>
-    <span class="console">${game.console}</span>
-    <a href="#" id="btnInfo" target="_blank">Saiba mais.</a>
-  `
+  games.forEach(game => {
+    // Verifica se o título do jogo contém a palavra pesquisada
+    if (
+      game.title.toLowerCase().includes(pesquisa) ||
+      game.description.toLowerCase().includes(pesquisa) ||
+      game.genre.toLowerCase().includes(pesquisa) ||
+      game.console.toLowerCase().includes(pesquisa)
+    ) {
+      // Cria um novo elemento de lista para o jogo
+      const listItem = document.createElement('li')
+      listItem.classList.add('item-resultado')
 
-  gameList.appendChild(listItem)
-})
+      // Define o conteúdo HTML do item da lista
+      listItem.innerHTML = `
+          <img src="${game.image}" alt="${game.title}" class="jogo-imagem">
+          <h2>${game.title}</h2>
+          <p>${game.description}</p>
+          <span class="genero">${game.genre}</span>
+          <span class="console">${game.console}</span>
+          <a href=${game.link} id="btnInfo" target="_blank">Saiba mais.</a>
+        `
+
+      // Adiciona o elemento à lista de resultados
+      resultados.push(listItem)
+    }
+  })
+
+  // Verifica se foram encontrados resultados
+  if (resultados.length === 0 || pesquisa == '') {
+    const nenhumResultado = document.createElement('li')
+    nenhumResultado.textContent =
+      'Nenhum resultado encontrado para a sua pesquisa.'
+    gameList.appendChild(nenhumResultado)
+  } else {
+    // Adiciona os resultados à lista HTML
+    resultados.forEach(item => {
+      gameList.appendChild(item)
+    })
+  }
+}
---
+++ data.js
@@ -0,0 +1,44 @@
+const games = [
+  {
+    title: 'Super Mario World',
+    description:
+      'Um clássico da Nintendo, explorando o Reino dos Cogumelos com o encanador mais famoso do mundo.',
+    image: './assets/marioworld.webp',
+    genre: 'Aventura',
+    console: 'Super Nintendo',
+    link: 'https://pt.wikipedia.org/wiki/Super_Mario_World'
+  },
+  {
+    title: 'Sonic the Hedgehog 2',
+    description:
+      'Acelere com o ouriço azul mais rápido do mundo em uma aventura épica.',
+    image: './assets/sonic2.webp',
+    genre: 'Aventura',
+    console: 'Mega Drive',
+    link: 'https://pt.wikipedia.org/wiki/Sonic_the_Hedgehog_2'
+  },
+  {
+    title: 'Street Fighter II',
+    description: 'Lutas de rua para provar os seus limites.',
+    image: './assets/street-fighter.webp',
+    genre: 'Luta',
+    console: 'Super Nintendo',
+    link: 'https://pt.wikipedia.org/wiki/Street_Fighter_II'
+  },
+  {
+    title: 'Zelda',
+    description: 'As aventuras de Link.',
+    image: './assets/zelda.png',
+    genre: 'Aventura',
+    console: 'Super Nintendo',
+    link: 'https://pt.wikipedia.org/wiki/Zelda'
+  },
+  {
+    title: 'Donkey Kong',
+    description: 'Bananas, aventuras e mais bananas.',
+    image: './assets/donkeykong.jpg',
+    genre: 'Aventura',
+    console: 'Super Nintendo',
+    link: 'https://pt.wikipedia.org/wiki/Donkey_Kong'
+  }
+]
